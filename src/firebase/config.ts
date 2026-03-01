91915ca0a97f47fc7e350364ca5405b00c1acf6a
Alberto Romero Castella <maritosecundario@gmail.com>
2026-03-01 12:47:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1848760758-930f1",
+  "appId": "1:814978435691:web:756ec412c143d167e43f2e",
+  "apiKey": "AIzaSyCw7ttErXkCRCNJgRtNsI5s0saVFpvWrHA",
+  "authDomain": "studio-1848760758-930f1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "814978435691"
+};
